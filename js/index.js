4f99380852f2065bf874505dae6b58f84de25139
David Lopez <dhelaman@hotmail.com>
2018-03-15 20:32:44 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function flip(){
+	document.getElementById("frontside").classList.toggle("front");
+    document.getElementById("backside").classList.toggle("back");
+}
+function changeClr( id ){
+    document.getElementById( id ).style.backgroundColor = "#3E5057";
+}
