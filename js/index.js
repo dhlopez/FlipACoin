4c97fd425406f3d727078be8a30b4f1ec29990a3
David Lopez <dhelaman@hotmail.com>
2018-03-18 23:48:20 -0400

    before moving to jquery

--- js/index.js
+++ js/index.js
@@ -1,7 +1,36 @@
 function flip(){
 	document.getElementById("frontside").classList.toggle("front");
     document.getElementById("backside").classList.toggle("back");
+    var frontside = document.getElementById("coins");
+frontside.addEventListener( 'webkitTransitionEnd', 
+    function( event ) { alert( "Finished transition!" ); }, false );
+    result();
 }
 function changeClr( id ){
     document.getElementById( id ).style.backgroundColor = "#3E5057";
 }
+
+var time = Math.random();
+var front = document.querySelector('.front');
+front.style.setProperty('--animation-time', time + 's');
+
+var back = document.querySelector('.back');
+back.style.setProperty('--animation-time', time + 's');
+
+function result()
+{
+	var x = (Math.floor(Math.random() * 2) == 0);
+	console.info(x);
+    if(x){
+    	//true, head
+    	document.getElementById("imgfront").classList.add("show");
+    	document.getElementById("imgfront").classList.remove("hide");
+    	document.getElementById("imgback").classList.add("hide");
+    }else{
+    	//false, tail
+        document.getElementById("imgfront").classList.add("hide");
+        document.getElementById("imgback").classList.add("show");
+        document.getElementById("imgback").classList.remove("hide");
+    }
+}
+
