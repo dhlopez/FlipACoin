c089b4a0f4ea9475a5bd26c73d9f7bb294fd535c
David Lopez <dhelaman@hotmail.com>
2018-03-23 00:42:17 -0400

    works on chrome, not on phonegap

--- js/index.js
+++ js/index.js
@@ -1,3 +1,53 @@
+function transit(){
+	var a = 1440;
+	$('#backside').show();
+	$('#frontside').show();
+
+	if(Math.floor(Math.random() * 2) == 0)
+	{
+		
+		$('#frontside').transition({
+	  	  //perspective: '50px',
+		  rotateY: '+='+a+'deg',
+		  duration: 4500,
+		  ease:'out'
+	    }, function(e)
+		  {
+			$('#backside').hide();
+		  });
+		$('#backside').transition({
+		  //perspective: '50px',
+		  rotateY: '+='+a+'deg',
+		  delay:150,
+		  duration: 4500,
+		  ease:'out'
+		});
+		console.info('head');
+		
+	}
+	else
+	{
+		$('#backside').transition({
+		  //perspective: '50px',
+		  rotateY: '+='+a+'deg',
+		  duration: 4500,
+		  ease:'out'
+		}, function(e)
+		  {
+			$('#frontside').hide();
+		  });
+		$('#frontside').transition({
+	  	  //perspective: '50px',
+		  rotateY: '+='+a+'deg',
+		  duration: 4500,
+		  delay:150,
+		  ease:'out'
+		});
+		console.info('tail');
+		
+	}
+}
+/*
 function flip(){
 	document.getElementById("frontside").classList.toggle("front");
     document.getElementById("backside").classList.toggle("back");
@@ -33,53 +83,5 @@ function result()
         document.getElementById("imgback").classList.remove("hide");
     }
 }
+*/
 
-function transit(){
-	var a = 1440;
-
-	if(Math.floor(Math.random() * 2) == 0)
-	{
-		$('#backside').show();
-		$('#frontside').show();
-		$('#frontside').transition({
-	  	  //perspective: '50px',
-		  rotateY: '+='+a+'deg',
-		  duration: 500,
-		  ease:'out'
-	    }, function(e)
-		  {
-			$('#backside').hide();
-		  });
-		$('#backside').transition({
-		  //perspective: '50px',
-		  rotateY: '+='+a+'deg',
-		  delay:150,
-		  duration: 500,
-		  ease:'out'
-		});
-		console.info('head');
-		
-	}
-	else
-	{
-		$('#backside').show();
-		$('#frontside').show();
-		$('#backside').transition({
-		  //perspective: '50px',
-		  rotateY: '+='+a+'deg',
-		  duration: 500,
-		  ease:'out'}, function(e)
-		  {
-			$('#frontside').hide();
-		  });
-		$('#frontside').transition({
-	  	  //perspective: '50px',
-		  rotateY: '+='+a+'deg',
-		  duration: 500,
-		  delay:150,
-		  ease:'out'
-		});
-		console.info('tail');
-		
-	}
-}
